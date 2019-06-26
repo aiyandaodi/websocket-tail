a9bcd06b470f313eed0df32fe362645900c5a7c8
zhangbin <529158785@qq.com>
2019-06-26 12:26:35 +0800

    张斌修改：     滚动条设置

--- src/main/webapp/show.js
+++ src/main/webapp/show.js
@@ -39,6 +39,8 @@ function getDockerLogs(){
 		// 接收服务端的实时日志并添加到HTML页面中
 		$("#log-container div").append(event.data);
 		// 滚动条滚动到最低部
+		var h = $(document).height()-$(window).height();
+        $(document).scrollTop(h);
 		$("#log-container").scrollTop($("#log-container div").height() - $("#log-container").height());
 	};
 }
